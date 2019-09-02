dfb936b6a496ab467b620fca79f8fc728498b60f
Sam Barker <samuel.e.barker@googlemail.com>
2019-09-02 11:30:57 +0100

    after lecture

--- 14-rails-rest/drinkers-paradise/app/controllers/bars_controller.rb
+++ 14-rails-rest/drinkers-paradise/app/controllers/bars_controller.rb
@@ -1,15 +1,43 @@
 class BarsController < ApplicationController
+
+  before_action :find_bar, only: [:show, :edit, :update, :destroy]
+
   def index
     @bars = Bar.all
   end
 
   def show
-    @bar = Bar.find params[:id]
   end
 
   def new
+    @bar = Bar.new
   end
 
   def create
+    bar = Bar.create(bar_params)
+    redirect_to bar
+  end
+
+  def edit
+  end
+
+  def update
+    @bar.update(bar_params)
+    redirect_to @bar
+  end
+
+  def destroy
+    @bar.destroy
+    redirect_to bars_path
+  end
+
+  private
+
+  def find_bar
+    @bar = Bar.find params[:id]
+  end
+
+  def bar_params
+    params.require(:bar).permit(:name, :location)
   end
 end
